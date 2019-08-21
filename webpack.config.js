a635f9cb38088956869b60c5162bed0177321d59
Alex <alxford45@gmail.com>
2019-08-21 17:24:01 -0500

    added comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+/**
+ * Webpack config file
+ *
+ * module.exports = {object} is the CommonJs export syntax which is used by NodeJs.
+ */
 module.exports = {
+  /**
+   * devServer specifies options for webpack-dev-server plugin
+   */
   devServer: {
-    port: 8080,
-    contentBase: "./src",
-    open: true
+    port: 8080, // Will run on localhost:8080
+    contentBase: "./src", // Serves files in src directory
+    open: true // Automattically opens the webpage in default browser
   }
 };
